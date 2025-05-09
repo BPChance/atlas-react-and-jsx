6ac8b87e64040529c37ded43db25597d22e6fc93
BPChance <bradenpchance6@gmail.com>
2025-05-09 18:07:16 -0500

    updated app with helpful resources component to provide helpful resources

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import Header from './components/Header';
 import Section from './components/Section';
+import HelpfulResource from './components/HelpfulResource';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Section title="What is react?">
-        React is a JavaScript library for building user interfaces. It allows for the creation of components that manage their own state to update the UI as changes are made. React uses a virtual DOM so the site doesn't have to reload everytime something is updated.
+        React is a JavaScript library for building user interfaces. It allows
+        for the creation of components that manage their own state to update the
+        UI as changes are made. React uses a virtual DOM so the site doesn't
+        have to reload everytime something is updated.
       </Section>
       <Section title="Benefits of react">
         <ul>
@@ -16,6 +20,19 @@ function App() {
           <li>Reusability</li>
           <li>Scalability & Maintanability</li>
         </ul>
+      </Section>
+      <Section title="Helpful Resources">
+        <HelpfulResource
+          label={'w3schools'}
+          link={'https://www.w3schools.com/react'}
+        />
+        <HelpfulResource
+          label={'React mdn web docs'}
+          link={
+            'https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Frameworks_libraries/React_getting_started'
+          }
+        />
+        <HelpfulResource label={'react dev'} link={'https://react.dev/'} />
       </Section>
     </div>
   );
